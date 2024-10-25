c2da6cb92dbb620a3f01c1db9d2367834af6961d
Alexey Drozdovskiy <Fixxy@users.noreply.github.com>
2024-10-25 12:59:27 +0200

    Revert "debug: log check_availability"
    This reverts commit a3898580d56cf73846885f84352c40cbe52acd47.

--- lib/carto/connector.rb
+++ lib/carto/connector.rb
@@ -118,9 +118,6 @@ module Carto
     end
 
     def self.limits(provider_name:, user:)
-      puts '>>>>>>>>>>>>> self.limits >>>>>>>>>>>>>>>>'
-      puts user.inspect
-      puts provider_name.inspect
       if configuration = user.connector_configuration(provider_name)
         { enabled: configuration.enabled?, max_rows: configuration.max_rows }
       else
