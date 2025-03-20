0a87bf8077dc889524460552e99be03e95afa125
Alexey Drozdovskiy <Fixxy@users.noreply.github.com>
2025-03-20 16:01:16 +0100

    Revert "debug: removed presenter_cache from index"
    This reverts commit fe8da6c9a0badf4aa93c15252d49259db07b7007.

--- app/controllers/carto/api/visualizations_controller.rb
+++ app/controllers/carto/api/visualizations_controller.rb
@@ -93,7 +93,8 @@ module Carto
 
         visualizations = vqb.with_order(order, order_direction)
                             .build_paged(page, per_page).map do |v|
-          VisualizationPresenter.new(v, current_viewer, self, presenter_options).to_poro \
+          VisualizationPresenter.new(v, current_viewer, self, presenter_options)
+                                .with_presenter_cache(presenter_cache).to_poro \
             unless params[:subscribed] == 'true' and not v.subscription.present?
         end.compact
 
